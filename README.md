f6f5fcf6b1739fd8995c4a513cdeec217be3bedb
Malelus <matin1608@gmail.com>
2021-02-03 19:41:00 +0100

    Major changes

---
+++ README.md
@@ -0,0 +1,61 @@
+# Strona z kartką walentynkową
+
+> ### [Link do strony](https://malelus.github.io/KartkaWalentynkowa/)
+
+---
+
+#### Strona została napisana w edytorze Visual Studio Code używając preprocesora SASS do pisania kodu CSS oraz użyto ikon ze strony https://fontawesome.com/.
+
+- czcionki:
+  - [Montserrat](https://fonts.google.com/specimen/Montserrat?query=mon).
+
+---
+
+### Obowiązkowe:
+
+- układ:
+
+  - logo,
+  - zawartość.
+
+- animacje:
+
+  - logo,
+  - kwadrat z imieniem.
+
+---
+
+### Dodatkowe:
+
+#### Budowa:
+
+- strona została zaprojektowana w sposób responsywny dzięki czemu umożliwia to swobodne i wygodne korzystanie z niej na każdym urządzeniu:
+
+| Smartphone                                                                 | Mobile                                                                | Desktop                                                                       |
+| -------------------------------------------------------------------------- | --------------------------------------------------------------------- | ----------------------------------------------------------------------------- |
+| < 699px                                                                    | 700px - 999px                                                         | > 1000px                                                                      |
+| widok przystowany do korzystania na smartphonie (pierwotny design strony), | widok mobilny, dostosowanie czcionek i animacji do większych ekranów, | widok desktop , dostosowanie czcionek i animacji do komputerów stacjonarnych, |
+
+- użyto znaczników semantycznych.
+
+#### Wygląd:
+
+- animacja wejścia na stronę wykonana przy pomocy CSS oraz JavaScript,
+
+- animowany link do repozytorium GitHub'a w lewym dolnym rogu strony,
+
+- zmieniony wygląd scrollbar'a.
+
+#### Dodatki:
+
+- informacje w konsoli o statusie animacji oraz o czasach trwania wszystkich animacji znajdujących się na stronie,
+
+- możliwość wyłączenia całkowicie animacji znajdujących się na stronie.
+
+  > (aby wyłączyć animacje: Ustawienia > Ułatwienia dostępu > Wyświetlacz > Pokaż animacje w systemie Windows (Wyłącz)) (Ustawienie znajduje się w pliku \_reset.scss).
+
+---
+
+#### Wykonał: Mateusz Narowski
+
+#### Data: 09.02.2021
--- addons/main.js
+++ addons/main.js
@@ -3,7 +3,11 @@ window.onload = () => {
   //===// Loading classes //===//
   //===========================//
 
-  // Empty
+  // - page transition
+  const transitionElementTrBl = document.querySelector('.transition-tr-bl');
+  const transitionElementTlBr = document.querySelector('.transition-tl-br');
+  const transitionElementBrTl = document.querySelector('.transition-br-tl');
+  const transitionElementBlTr = document.querySelector('.transition-bl-tr');
 
   //=============================//
   //===// Loading variables //===//
@@ -16,10 +20,12 @@ window.onload = () => {
   const durationsArray = [];
   const varArray = [
     '--btn-anim-time',
+    '--transition-time',
     '--logo-anim-time',
     '--heart-show',
     '--heart-beat',
     '--anim-delay',
+    '--all-anim-delay',
   ];
 
   // Defining const for global variables source and quantity of variable array
@@ -39,6 +45,9 @@ window.onload = () => {
   for (let i = 0; i < varQuantity; i++) {
     durationsArray[i] = durationsArray[i].substring(0, durationsArray[i].length - 1);
   }
+
+  // Setting own variable for transition time
+  const timeTransition = durationsArray[0] * 1000;
 
   // Setting style for console timing info
   for (let i = 0; i < varQuantity; i++) {
@@ -75,10 +84,34 @@ window.onload = () => {
     console.log(`# Scripts were loaded correctly! (Time is given in seconds)`);
     console.log(`# Animations are ` + motionStatus + `\n\n`);
     console.log(`# Button animation time: ` + durationsArray[0]);
-    console.log(`# Logo animation time: ` + durationsArray[1]);
+    console.log(`# Transition time: ` + durationsArray[1]);
+    console.log(`# Logo animation time: ` + durationsArray[2]);
     console.log(`# Heart animation:`);
-    console.log(`  - show: ` + durationsArray[2]);
-    console.log(`  - beat: ` + durationsArray[3]);
-    console.log(`# Animation delay: ` + durationsArray[4]);
+    console.log(`  - show: ` + durationsArray[3]);
+    console.log(`  - beat: ` + durationsArray[4]);
+    console.log(`# Delays:`);
+    console.log(`  - anim: ` + durationsArray[5]);
+    console.log(`  - all: ` + durationsArray[6]);
   }
+
+  //====================//
+  //===// Scripts //===//
+  //===================//
+
+  //===// Page transition //===//
+  setTimeout(() => {
+    transitionElementTrBl.classList.remove('transition--active-tr-bl');
+  }, timeTransition);
+
+  setTimeout(() => {
+    transitionElementTlBr.classList.remove('transition--active-tl-br');
+  }, timeTransition);
+
+  setTimeout(() => {
+    transitionElementBrTl.classList.remove('transition--active-br-tl');
+  }, timeTransition);
+
+  setTimeout(() => {
+    transitionElementBlTr.classList.remove('transition--active-bl-tr');
+  }, timeTransition);
 };
